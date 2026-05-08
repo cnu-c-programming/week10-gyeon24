c58cb444616a49d3ad8e22287c156116e4c5a973
gyeon24 <162951845+gyeon24@users.noreply.github.com>
2026-05-08 09:29:40 +0900

    Update 1-1.c

--- code/1-1.c
+++ code/1-1.c
@@ -10,11 +10,11 @@ struct S2 {
     char c;
     int i;
     long long l;
-} __attribute__((packed)); [span_3](start_span)// gcc 문법, #pragma pack(1)로 대체 가능[span_3](end_span)
+} __attribute__((packed)); [span_3](start_span)
 
 int main() {
     struct S1 s1;
     struct S2 s2;
-    printf("%zu %zu\n", sizeof(s1), sizeof(s2)); [span_4](start_span)//[span_4](end_span)
+    printf("%zu %zu\n", sizeof(s1), sizeof(s2));
     return 0;
 }
