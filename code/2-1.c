79995a5bb4bccb2de036fe62b6c43be8957a2f5d
gyeon24 <162951845+gyeon24@users.noreply.github.com>
2026-05-08 09:34:01 +0900

    Update 2-1.c

--- code/2-1.c
+++ code/2-1.c
@@ -3,14 +3,14 @@
 union U {
     float f;
     unsigned char c[4];
-}; [span_17](start_span)//[span_17](end_span)
+};
 
 int main() {
     union U u;
-    u.f = 3.14f; [span_18](start_span)//[span_18](end_span)
+    u.f = 3.14f;
     
     for (int i = 0; i < 4; i++) {
-        printf("%x\n", (unsigned)u.c[i]); [span_19](start_span)//[span_19](end_span)
+        printf("%x\n", (unsigned)u.c[i]);
     }
     
     return 0;
