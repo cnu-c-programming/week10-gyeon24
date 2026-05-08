058b4a07628a070e7fa8f4a9114939b969863fee
gyeon24 <162951845+gyeon24@users.noreply.github.com>
2026-05-08 09:34:26 +0900

    Update 2-2.c

--- code/2-2.c
+++ code/2-2.c
@@ -4,21 +4,21 @@ struct access_rights {
     unsigned char execute : 1;
     unsigned char write : 1;
     unsigned char read : 1;
-}; [span_21](start_span)//[span_21](end_span)
+};
 
 union file_access_rights {
     unsigned int int_value;
     struct access_rights rights;
-}; [span_22](start_span)//[span_22](end_span)
+};
 
 int main() {
     union file_access_rights some_file = {0};
     
     some_file.rights.read = 1;
     some_file.rights.write = 0;
-    some_file.rights.execute = 1; [span_23](start_span)//[span_23](end_span)
+    some_file.rights.execute = 1;
     
-    printf("current mode: %u\n", some_file.int_value); [span_24](start_span)//[span_24](end_span)
+    printf("current mode: %u\n", some_file.int_value);
     
     return 0;
 }
